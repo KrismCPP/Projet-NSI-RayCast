370743fe7fd5f696d98b343248acd0e8ecdffc10
PeppAshPig <83552220+PeppAshPig@users.noreply.github.com>
2022-04-10 19:19:12 +0200

    Update TwoDRaycast.py

--- TwoDRaycast.py
+++ TwoDRaycast.py
@@ -1,41 +1,51 @@
+"""Rendu 2D - Fonctions et Classes Utiles"""
+
+
+'''IMPORTATION DES MODULES'''
+
 from Utils import *
 from graphic_mansart import *
 from math import floor
 import pygame
 
-#Vector = Point
 
-pygame.init()
+'''Classes'''
 
 class Entity :
-    def __init__(self,x,y,map,angle) :
+    def __init__(self,x,y,map,angle,window) :
         self.pos = Vector2D(x,y) #Position du Joueur
         self.angle = angle #Direction où pointe Joueur
         self.dir = rotateVector(Vector2D(1,0),angle*math.pi/180) #Vecteur du directeur par rapport à la direction où pointe Joueur
         self.map = map
+        self.window = window
 
-    def Move(self,speed) :
+class Player(Entity):
+    """ Classe fille de Entity"""
+    def __init__(self,fov,angle,x,y,map,window) :
+        self.fov = fov #Champ de vision du Joueur
+        super().__init__(x,y,map,angle,window)
+
+
+    def move(self,speed) :
         """ Déplacer le joueur selon la distance donnée sur l'axe y :
         Avancer : speed > 0
         Reculer : speed < 0
         """
-        self.pos.x += self.dir.x * speed
-        self.pos.y += self.dir.y * speed
+        if speed > 0:
+            if self.distance(self.findWall(rotateVector(Vector2D(1,0),(self.angle+90)*math.pi/180))) > 1 and \
+                    self.distance(self.findWall(rotateVector(Vector2D(1,0),(self.angle-90)*math.pi/180))) > 1 and \
+                    self.distance(self.findWall(rotateVector(Vector2D(1,0),(self.angle)*math.pi/180))) - self.dir.x * speed > 6:
+                self.pos.x += self.dir.x * speed
+                self.pos.y += self.dir.y * speed
+        if speed < 0:
+            if self.distance(self.findWall(rotateVector(Vector2D(-1,0),(self.angle+90)*math.pi/180))) > 1 and \
+                    self.distance(self.findWall(rotateVector(Vector2D(-1,0),(self.angle-90)*math.pi/180))) > 1 and \
+                    self.distance(self.findWall(rotateVector(Vector2D(-1, 0), (self.angle) * math.pi / 180))) - self.dir.x * speed > 6 :
+                self.pos.x += self.dir.x * speed
+                self.pos.y += self.dir.y * speed
 
-    def distance(self, Vector) -> float:
-        """ Renvoie la distance entre la pos du joueur et le point Vector"""
-        return math.sqrt((self.pos.x - Vector.x)**2 + (self.pos.y - Vector.y)**2)
 
-
-class Player(Entity):
-    def __init__(self,fov,angle,x,y,map,window) -> None:
-        self.fov = fov #Champ de vision du Joueur
-        self.window = window #Fenêtre Graphique
-        super().__init__(x,y,map,angle)
-
-    
-
-    def Rotate(self, angle) :
+    def rotate(self, angle) :
         """ Modifie où pointe le joueur selon l'angle donnée :
         Gauche : angle < 0
         Droite : angle > 0
@@ -43,10 +53,15 @@ class Player(Entity):
         self.angle += angle
         self.dir = rotateVector(Vector2D(1,0),self.angle*math.pi/180) #Vecteur du directeur par rapport à la direction où pointe Joueur
 
+
+    def distance(self, Vector) -> float:
+        """ Renvoie la distance entre la pos du joueur et le point Vector"""
+        return math.sqrt((self.pos.x - Vector.x)**2 + (self.pos.y - Vector.y)**2)
+
+
     def drawRay(self, Vector) :
         """ Trace une ligne entre la pos du joueur et le point Vector """
         pygame.draw.line(self.window,pygame.Color(0,0,255),(self.pos.x, self.pos.y),(Vector.x,Vector.y),1)
-
 
 
     def findWall(self,Vector) :
@@ -119,6 +134,7 @@ class Player(Entity):
         else :
             return Vector2D(x1,y1)
 
+
     def scanWalls(self) -> list :
         """
         Renvoie la distance des différents murs dans son champ de vision
@@ -137,6 +153,7 @@ class Player(Entity):
             walls += [self.distance(self.findWall(newVector))]
         return walls
 
+
     def drawAllRays(self) :
         """
         Trace les différents vecteurs entre le joueur et les murs
@@ -152,7 +169,63 @@ class Player(Entity):
 
 
 
+class Monster (Entity) :
+    """ Classe fille de Entity"""
 
+    def __init__(self,angle,pos_graph,pos_list,map,window) :
+        self.pos_list = pos_list
+        super().__init__(pos_graph[0],pos_graph[1],map,angle,window)
+
+    def move(self, new_pos) :
+        """ Prend en argument un tuple
+        Déplace l'indice du Monstre à celle indiquée"""
+        self.pos_list = new_pos
+
+    def path_finding(self,pos_player):
+        """
+        Prend en arguement la position du joueur sous forme de tuple
+        Renvoie une liste de plusieurs tuple contenant le chemin pour aller jusqu'au joueur
+        """
+        # Adapte les coordonnées du joueur par rapport a la liste de liste 'map'
+        pos_player = (floor(pos_player[1]/ 480 * len(self.map)), floor(pos_player[0]/ 480 * len(self.map)))
+
+        paths_possibilities = [self.pos_list] # Stockera tous les chemins enivisageable au fur et à mesure
+        visited = {} #Dictionnaire pour conserver les positions visitées
+
+        while len(paths_possibilities) != 0: # Tant qu'il reste un chemin possible
+
+            # On traite un des chemins possibles
+            if paths_possibilities[0] == self.pos_list:
+                path = [paths_possibilities.pop(0)]
+            else:
+                path = paths_possibilities.pop(0)
+
+            x,y = path[-1] # Assigne à x,y les dernières valeurs du chemin en construction
+
+            if (x,y) == pos_player: # Vérifie si ce chemin atteint la destination
+                path.pop(0)
+                return path
+
+            elif (x,y) not in visited: # Recherche si la position n'est pas encore visitée
+
+                '''Recherche des directions possibles'''
+                coord_to_check = [ (x+1,y), (x-1,y), (x,y+1), (x,y-1)] # 4 Directions
+                next_pos = [] # Var pour stocker les nouvelles positions possibles
+                for next_coord in coord_to_check:
+                    if self.map[next_coord[0]][next_coord[1]] != '#' and next_coord not in visited:
+                        next_pos.append(next_coord)
+
+                '''Stockage du nouveau chemin avec les autres possibilités'''
+                for coord in next_pos:
+                    newPath = [path]
+                    newPath.append(coord)
+                    paths_possibilities.append(newPath)
+
+                visited[(x,y)] = None # Ajout de cette position à celles déjà visitées
+
+
+
+'''Fonctions'''
 
 
 def displayAll(map,player : Player,window) :
@@ -169,45 +242,32 @@ def displayAll(map,player : Player,window) :
     pygame.draw.circle(window,pygame.Color(255,0,0),(int(player.pos.x),int(player.pos.y)),5)
     player.drawAllRays()
 
-            
-
-        i = 0
-
-        cadrex = floor(posx/ 480 * len(self.map))
-        cadrey = floor(posy/ 480 * len(self.map))
-
-        while self.map[cadrey][cadrex] != "#" :
-            i += 1
-                
-            facteurx = floor(posx+i)-posx
-
-            x2 = Vector.x*facteurx+posx
-            y2 = Vector.y*facteurx+posy
-
-            cadrex = floor(x2/ 480 * len(self.map))
-            cadrey = floor(y2/ 480 * len(self.map))
-            if self.map[cadrey][cadrex] == "#" :
-
-                distancey = math.sqrt((posx - x2)**2 + (posy - y2)**2)
-                
-
-        if distancex > distancey :
-            return Vector2D(x2,y2)
-        else : 
-            return Vector2D(x1,y1)
-    
-    def drawAllRays(self) :
-        for i in range(-self.fov//2, self.fov//2) :
-            
-            angle = i * math.pi/180
-
-            newVector = rotateVector(self.dir,angle)
-
-            self.drawRay(self.findWall(newVector))
-            
 
 
-    
+    i = 0
+
+    cadrex = floor(posx/ 480 * len(self.map))
+    cadrey = floor(posy/ 480 * len(self.map))
+
+    while self.map[cadrey][cadrex] != "#" :
+        i += 1
+
+        facteurx = floor(posx+i)-posx
+
+        x2 = Vector.x*facteurx+posx
+        y2 = Vector.y*facteurx+posy
+
+        cadrex = floor(x2/ 480 * len(self.map))
+        cadrey = floor(y2/ 480 * len(self.map))
+        if self.map[cadrey][cadrex] == "#" :
+
+            distancey = math.sqrt((posx - x2)**2 + (posy - y2)**2)
+
+
+    if distancex > distancey :
+        return Vector2D(x2,y2)
+    else :
+        return Vector2D(x1,y1)
 
 
 def displayAll(map,player,window) :
@@ -219,4 +279,3 @@ def displayAll(map,player,window) :
                 pygame.draw.rect(window,pygame.Color(255,255,255),(480/len(map)*j,480/len(map)*i,int(480/len(map)),int(480/len(map))))
     pygame.draw.circle(window,pygame.Color(255,0,0),(int(player.pos.x),int(player.pos.y)),5)
     player.drawAllRays()
-    
